75649e243cb02b5df32628778d1583dc6119352a
Thomas Pommerening <pommermann@hotmail.de>
2017-11-09 17:34:02 +0100

    Moved RayCastpart from PlayerMovement to RaycastHandler + Added PlayerSelect per Raycast

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,40 +6,18 @@ using UnityEngine.AI;
 public class PlayerMovement : MonoBehaviour {
 
     private NavMeshAgent agent;
-    private int floorMask;
-    private Ray ray;
-    private RaycastHit rayHit;
+    
     // Use this for initialization
     void Start () {
         agent = GetComponent<NavMeshAgent>();
-        floorMask = LayerMask.GetMask("Floor");
     }
 	
 	// Update is called once per frame
 	void Update () {
-        getInputPosition();
+        
 	}
-    private void getInputPosition()
-    {
-#if UNITY_EDITOR
-        if (Input.GetMouseButton(0))
-        {
-            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        }
-#else
-        ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
 
-#endif
-        if(Physics.Raycast(ray,out rayHit, 100f, floorMask))
-        {
-            GoToPoint(rayHit.point);
-        }
-       /* else if(Physics.Raycast(ray, out rayHit, 100f, 8))
-        {
-
-        }*/
-    }
-    private void GoToPoint(Vector3 point)
+    public void GoToPoint(Vector3 point)
     {
         agent.SetDestination(point);
     }
---
+++ Assets/Scripts/RaycastHandler.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastHandler : MonoBehaviour {
+
+    private int floorMask;
+    private int playerMask;
+    private Ray ray;
+    private RaycastHit rayHit;
+    private GameObject activePlayer;
+    // Use this for initialization
+    void Start () {
+        floorMask = LayerMask.GetMask("Floor");
+        playerMask = LayerMask.GetMask("Player");
+        activePlayer = GameObject.FindGameObjectWithTag("ActivePlayer");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        getInputPosition();
+	}
+    private void getInputPosition()
+    {
+#if UNITY_EDITOR
+        if (Input.GetMouseButton(0))
+        {
+            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        }
+#else
+        ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+
+#endif
+        
+        if(Physics.Raycast(ray, out rayHit, 100f, playerMask))
+        {
+            GameObject hitObject = rayHit.transform.gameObject;
+            Debug.Log("Dont Hit me");
+            if (hitObject.tag == "Player")
+            {
+                activePlayer.tag = "Player";
+                activePlayer = hitObject;
+                activePlayer.tag = "ActivePlayer";
+            }
+        }
+        else if (Physics.Raycast(ray, out rayHit, 100f, floorMask))
+        {
+            activePlayer.GetComponent<PlayerMovement>().GoToPoint(rayHit.point);
+        }
+    }
+}

7c741714e460e51d1c2cfcf99c6344d579d83389
Thomas Pommerening <pommermann@hotmail.de>
2017-11-10 17:22:04 +0100

    Added GuestPrefab + GuestBehaviour Script + Added entrance and Spawnpoint to Map + Added WaiterGame Script as Overseer

---
+++ Assets/Scripts/GuestBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class GuestBehaviour : MonoBehaviour {
+
+    private NavMeshAgent agent;
+
+
+    void Awake () {
+        agent = GetComponent<NavMeshAgent>();
+	}
+	
+	
+	void Update () {
+		
+	}
+    public void SetChair(Vector3 destinitionPoint)
+    {
+        agent.SetDestination(destinitionPoint);
+    }
+}
---
+++ Assets/Scripts/WaiterGame.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaiterGame : MonoBehaviour {
+
+    public Transform[] chairArray;
+    public GameObject guestPrefab;
+    public Transform guestSpawnPoint;
+
+    private bool[] freeChair;
+
+    private void Start () {
+
+        freeChair = new bool[chairArray.Length];
+        for (int i = 0; i < freeChair.Length; i++)
+        {
+            freeChair[i] = true;
+        }
+        SpawnGuest();
+
+    }
+	
+	private void Update () {
+		
+	}
+    private void SpawnGuest()
+    {
+        GameObject guest = Instantiate(guestPrefab, guestSpawnPoint);
+        int chairInt = (int)Random.Range(0, chairArray.Length);
+        guest.GetComponent<GuestBehaviour>().SetChair(chairArray[CheckChair(chairInt)].position);
+    }
+
+    private int CheckChair(int chairInt)
+    {
+        if (freeChair[chairInt])
+        {
+            freeChair[chairInt] = false;
+            return chairInt;
+        }
+        else
+        {
+            if (chairInt == 0)
+            {
+                return CheckChair(freeChair.Length - 1);
+            }
+            else
+            {
+                return CheckChair(chairInt - 1);
+            }
+
+        }
+    }
+}
